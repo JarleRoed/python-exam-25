f39c246efde5c4598e7b85ca87077804493e5258
JarleRoed <jarlerood@gmail.com>
2026-05-11 00:09:42 +0200

    change_guessLetter

--- T1.py
+++ T1.py
@@ -10,27 +10,41 @@ with open('./data/ListWordGame', 'r') as impFile:
     print("-      Welcome to the word game      -")
     print("--------------------------------------")
 
+#need to change from correct word to correct letter.
 for iWord, word in enumerate(impLines, 1):
     # guesses per letter:
     numGuess = len(word)
+    lettGuess = []
 
     while numGuess > 0:
-        guess = input(f"Guess word nr {iWord}. You have {numGuess} guesses.")
+
+        wordDisplay = ""
+        for letter in word:
+            if letter in lettGuess:
+                wordDisplay += letter + " "
+            else:
+                wordDisplay += "_ "
+        print(f"\nCurrent word is {iWord}: {wordDisplay}\n")
+        print(f"You have {numGuess} guesses left")
+
+        guess = input("Guess a letter: ")
 
         if guess == '':
             print("Please give a valid input")
-        elif guess == word:
-            print("You guessed the word!")
-            iWord += 1
-            break
-        else:
-            print("You guessed wrong. Please try again.")
-            numGuess -= 1
-            print(f"The word is {word}")
-
-            if numGuess == 0:
-                print("You ran out of guesses for this word.")
-                print(f"The word was {word}.")
+        elif len(guess) > 1:
+            print("One letter at a time")
+        elif guess.lower in lettGuess:
+            print("You already guessed this letter")
+        elif guess.lower() in word.lower():
+            print("Correct letter!")
+            lettGuess.append(guess.lower())
+            if all(letter.lower() in lettGuess for letter in word):
+                print(f"You guessed the word: {iWord}")
                 break
-print("You won the game!")
 
+        else:
+            print("Wrong letter")
+            numGuess -= 1
+            if numGuess == 0:
+                print(f"You ran out of guesses. The word was {word}")
+print("You won the game!")
