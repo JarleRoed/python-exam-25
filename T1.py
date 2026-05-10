4264059fbb872a9f5836f6df4c2439fa8e208d80
JarleRoed <jarlerood@gmail.com>
2026-05-10 22:09:29 +0200

    base_complete

--- T1.py
+++ T1.py
@@ -2,33 +2,36 @@
 
 #reading file into list
 with open('./data/ListWordGame', 'r') as impFile:
-    impLines = impFile.readlines()
-
-for i, word in enumerate(impLines, 1):
-    print("word", iWord, word)
+    impLines = [line.strip() for line in impFile]
 
 #main
 
-numGuess = 0
 #program interface
-while True:
     print("--------------------------------------")
     print("-      Welcome to the word game      -")
     print("--------------------------------------")
 
+for iWord, word in enumerate(impLines, 1):
+    # guesses per letter:
+    numGuess = len(word)
 
     while numGuess > 0:
         guess = input(f"Guess word nr {iWord}. You have {numGuess} guesses.")
+
         if guess == '':
             print("Please give a valid input")
         elif guess == word:
             print("You guessed the word!")
+            iWord += 1
             break
         else:
             print("You guessed wrong. Please try again.")
             numGuess -= 1
+            print(f"The word is {word}")
+
             if numGuess == 0:
                 print("You ran out of guesses for this word.")
                 print(f"The word was {word}.")
-
+                break
+print("You won the game!")
 #matching iGuess
