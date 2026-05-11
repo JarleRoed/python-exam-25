0872b9b1bc1053f9801dae287e80ed5e32bb708b
JarleRoed <jarlerood@gmail.com>
2026-05-11 02:35:03 +0200

    T2_fix_functions

--- T2.py
+++ T2.py
@@ -1,22 +1,50 @@
 #library management system
 
 class Book:
-    def __init__(self, title, author, num_pages):
+    def __init__(self, title, author, num_pages, num_copy):
         self.title = title
         self.author = author
         self.num_pages = num_pages
+        self.num_copy = num_copy
 
 class Library:
     def __init__(self):
         self.booksList = [] #empty list
 
-    def add_book(self, title, author, num_pages):
-        book = Book(title, author, num_pages)
+    def add_book(self, title, author, num_pages, num_copy):
+        book = Book(title, author, num_pages, num_copy)
         self.booksList.append(book)
+        book.num_copy += 1
+
+    def remove_book(self, title):
+        for book in self.bookList:
+            if book.title == title:
+                self.booksList.remove(book)
+                book.num_copy - 1
+                print(f"{title} is removed")
+                return
+        print("Book was not found!")
+
+    def check_in(self, title):
+        for book in self.booksList:
+            if book.title == title:
+                book.num_copy += 1
+                print(f"Checked in {title}: Copies in available {book.num_copy}")
+
+    def check_out(self, title):
+        for book in self.booksList:
+            if book.title == title:
+                if book.num_copy > 0:
+                    book.num_copy -= 1
+                    print(f"Checked out {title}. Copies left: {book.num_copy}")
+                else:
+                    print("There are no copies available.")
+                return
+            print("Book not found.")
 
     def showListBooks(self):
         for book in self.booksList:
-            print(f"{book.title} : {book.author} : {book.num_pages}")
+            print(f"{book.title} : {book.author} : {book.num_pages} pages : {book.num_copy} copies")
 
 #program:
 
@@ -27,10 +55,26 @@ print("------------------------")
 lib = Library()
 
 while True:
+
+    print("\n1. Show list of all books in the library:")
+    print("2. Add a book")
+    print("3. ")
+    print("")
+    print("")
+    print("")
+
+
+
     title = input("Enter the title of the book: ")
+
     author = input("Enter name of author: ")
+
     num_pages = input("Enter number of pages: ")
     lib.add_book(title, author, num_pages)
+
+    rmBook = input("Enter book you want to remove from library system: ")
+
+
 
     quit = input("Type Quit to end program")
     if quit == "Quit":
